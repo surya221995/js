d922a1e9dd4f354ea7f8fcb1dc35777a1ed72590
Surya <kamblesuryaprakash.8@gmail.com>
2024-07-25 22:51:21 +0530

    firstjs project

---
+++ first.js
@@ -0,0 +1,18 @@
+// console.log("hello world"+a)
+// const a=1
+//to install extension internet should be on
+//install extension -----JavaScript (ES6) code snippets ,code runner 
+//ctl+alt+n :-to run Code
+//ctl+alt+m :- to stop run
+//to take inputs from user insatll package :----npm install prompt-sync
+
+//how to create .gitignore file
+
+
+//git bash : 
+//Git Bash is an application for Microsoft Windows environments which provides an emulation layer for a Git command line experience. Bash is an acronym for Bourne Again Shell. A shell is a terminal application used to interface with an operating system through written commands.//
+
+const prompt = require('prompt-sync')();
+var fname = prompt("Enter your name :");
+var lname = prompt("Enter your Last name :");
+console.log("your full name is :",fname,lname);
